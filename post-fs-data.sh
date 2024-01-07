0aae7b7e377b4ec9dbc373b9e294461b8ca50f07
nakixii <61456408+nakixii@users.noreply.github.com>
2024-01-07 12:34:32 +0800

    Add support for mounting to the same directory in multiple instances

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -2,15 +2,14 @@ MODDIR=${0%/*}
 TMPDIR="/dev/anyfs"
 PATH="/system/bin:$PATH"
 
-if [ -z "`mkdir $TMPDIR 2>&1`" ]; then
-     mount -t tmpfs anyfs $TMPDIR
-     mkdir  $TMPDIR/upper $TMPDIR/work
-fi
+mkdir -p $TMPDIR
+mount -t tmpfs anyfs -o remount $TMPDIR
 
-for i in "`ls $MODDIR/anymount`"; do
-    if [ -z "`mkdir $TMPDIR/work/$i 2>&1`" ]; then
-        cp -r $MODDIR/anymount/$i $TMPDIR/upper/$i
-        mount -t overlay anyfs -o lowerdir=/$i,upperdir=$TMPDIR/upper/$i,workdir=$TMPDIR/work/$i /$i
-        restorecon -R /$i
-    fi
+for i in `ls $MODDIR/anymount`; do
+    [ -d $TMPDIR/upper/$i ] && flag="remount,"
+    mkdir -p $TMPDIR/upper/$i $TMPDIR/work/$i
+    cp -a $MODDIR/anymount/$i/* $TMPDIR/upper/$i
+
+    mount -t overlay anyfs -o "$flag"lowerdir=/$i,upperdir=$TMPDIR/upper/$i,workdir=$TMPDIR/work/$i /$i
+    restorecon -R /$i
 done
