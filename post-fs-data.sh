6a69f9af0e541fb089e4ef8ce2f4b2e0a9df433f
nakixii <61456408+nakixii@users.noreply.github.com>
2024-01-14 10:33:28 +0800

    Use bind mounts

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,12 +1,5 @@
-MODDIR=${0%/*}
-TMPDIR="/dev/anyfs"
-PATH="/system/bin:$PATH"
-
-for i in `ls $MODDIR/anymount`; do
-    [ -d $TMPDIR/upper/$i ] && flag="remount,"
-    mkdir -p $TMPDIR/upper/$i $TMPDIR/work/$i
-    cp -a $MODDIR/anymount/$i/* $TMPDIR/upper/$i
-
-    mount -t overlay anyfs -o "$flag"lowerdir=/$i,upperdir=$TMPDIR/upper/$i,workdir=$TMPDIR/work/$i /$i
-    restorecon -R /$i
+TMPDIR=${0%/*}/anymount
+for i in `/bin/find $TMPDIR -type f -printf "%P "`; do
+    /bin/mount /$TMPDIR/$i /$i
+    restorecon /$i
 done
